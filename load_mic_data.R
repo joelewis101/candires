8d2d287ba5d55ec137a7a6765c3c579af535fbd7
Joe Lewis <joelewis101@doctors.org.uk>
2025-01-16 14:41:41 +0000

    draft final analysis with final data

--- load_mic_data.R
+++ load_mic_data.R
@@ -3,14 +3,14 @@ require(tidyverse)
 require(here)
 # split off micr measurements and drugs in to seperate dataframes 
 
-path <- here("data-raw/long_df_mic_160124.csv")
+path <- here("data-raw/iso_afg_clean.csv")
 
 df_raw <- read_csv(path)
 
-df_mics <-
-  df_raw |>
-  select(c(psn, date_enrol, swb_date, isol_species, starts_with("mic"))) |>
-  select(-mic_tested) |>
+df_mics <- 
+df_raw |>
+  select(c(psn, date_enrol, isol_date, isol_species, starts_with("mic"))) |>
+  # select(-mic_tested) |>
   unique() |>
   pivot_longer(
     cols = starts_with("mic"),
@@ -19,7 +19,8 @@ df_mics <-
     values_to = "mic",
     values_drop_na = TRUE
   ) |>
-  rename(species = isol_species) 
+  rename(species = isol_species,
+  swb_date = isol_date) 
 
 df_drug <-
   df_raw |>
@@ -31,8 +32,13 @@ df_drug <-
     date_antifung_end
   ) |>
   filter(!is.na(antifung_class)) |>
+  filter(antifung_class != "Unexposed to any antifungal") |>
   unique() |>
   mutate(date_antifung_start = date(date_antifung_start))
+
+df_raw <-
+df_raw |>
+  rename(swb_date = isol_date)
 
 cat("\n")
 cat("---------------------------\n")
